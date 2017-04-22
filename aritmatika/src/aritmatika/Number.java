090b89e79db39c9b9cbe017e6339462c70224ce8
FOrifki <rifkimazhar@gmail.com>
2017-04-22 09:57:14 +0700

    menambah branch baru

---
+++ aritmatika/src/aritmatika/Number.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika;
+
+/**
+ *
+ * @author FO
+ */
+abstract class Number {
+    int x;
+    int y;
+}
---
+++ aritmatika/src/aritmatika/Operation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika;
+
+/**
+ *
+ * @author FO
+ */
+public interface Operation {
+    public float operate(int x, int y);
+    public void display();
+}
